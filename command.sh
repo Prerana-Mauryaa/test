7669558ddccdc671d16dd9bcd44568ef9fafc169
Prerana Maurya <preranamaurya012389@gmail.com>
2025-02-04 19:36:19 +0530

    push

--- command.sh
+++ command.sh
@@ -6,7 +6,7 @@ helm upgrade --install business-service ./business-service --namespace business-
 --set stable.image.tag=v5
 
 helm upgrade --install business-service ./business-service --namespace business-service \
---set canary.image.tag=v6 \
+--set canary.image.tag=v7 \
 --set canary.replicaCount=1
 
 helm template ./business-service --namespace business-service --set stable.image.tag=v5
@@ -15,5 +15,7 @@ helm template ./business-service --namespace business-service --set stable.image
 helm upgrade --install canary ./canary --namespace business-service \
 --set defaults.host=business-service.business-service.svc.cluster.local \
 --set defaults.namespace=business-service \
---set trafficWeights.stable=90 \
---set trafficWeights.canary=10
+--set trafficWeights.stable=0 \
+--set trafficWeights.canary=100
+
+
